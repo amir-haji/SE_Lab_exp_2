bd4201305adc03c836db9534b32912d14c0cfc22
Aquasar <a.razghandi11@gmail.com>
2024-07-20 22:33:31 +0330

    Modify the Main.java File base on the requirements

--- Step_3_With_SOLID/src/Main.java
+++ Step_3_With_SOLID/src/Main.java
@@ -1,68 +1,67 @@
-import PaymentServices.OnSiteOrderService;
-import PaymentServices.OnlineOrderService;
-import PaymentServices.OrderService;
-import PaymentServices.PhoneOrderService;
+package PhaseOne;
+
+import PhaseOne.PaymentServices.OnSiteOrderService;
+import PhaseOne.PaymentServices.OnlineOrderService;
+import PhaseOne.PaymentServices.OrderService;
+import PhaseOne.PaymentServices.PhoneOrderService;
 
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scanner= new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         OrderService orderService = null;
         String customerName;
         Order order;
-        int customerAnswerForOrder=0;
-        int customerAnswerForPaymentMethod=0;
+        int customerAnswerForOrder = 0;
+        int customerAnswerForPaymentMethod = 0;
 
         System.out.println("Enter Customer Name : ");
         customerName = scanner.nextLine();
         order = new Order(customerName);
 
         //Step 1 : Select Order Items
-        while (customerAnswerForOrder!=3){
+        while (customerAnswerForOrder != 3) {
             System.out.println("For Ordering Sandwich enter 1.");
             System.out.println("For Ordering Pizza enter 2.");
             System.out.println("For submit your order enter 3");
             customerAnswerForOrder = scanner.nextInt();
 
-            if(customerAnswerForOrder==1){
-                order.addItem(new Food("sandwich",1000));
-            } else if(customerAnswerForOrder==2){
-                order.addItem(new Food("pizza",2000));
+            if (customerAnswerForOrder == 1) {
+                order.addItem(new Food("sandwich", 1000));
+            } else if (customerAnswerForOrder == 2) {
+                order.addItem(new Food("pizza", 2000));
             }
-
-
         }
-
         //Step2 : Select Payment Method
-        System.out.println("Enter Your Payment Method (1 for online and 2 for on-site and 3 for on-phone):");
+        System.out.println("Select Your Payment Method:");
+        System.out.println("1 for online");
+        System.out.println("2 for on-site");
+        System.out.println("3 for phone order");
+
         customerAnswerForPaymentMethod = scanner.nextInt();
-        if(customerAnswerForPaymentMethod==1){
-            orderService = new OnlineOrderService();
-            orderService.onlineOrderRegister(customerName);
-        } else if(customerAnswerForPaymentMethod==2){
-            orderService = new OnSiteOrderService();
-            orderService.onSiteOrderRegister(customerName);
-        } else if(customerAnswerForPaymentMethod==3){
-            orderService = new PhoneOrderService();
-            orderService.phoneOrderRegister(customerName);
+        String orderType;
+        switch (customerAnswerForPaymentMethod) {
+            case 1:
+                orderType = "online";
+                break;
+            case 2:
+                orderType = "onsite";
+                break;
+            case 3:
+                orderType = "phone";
+                break;
+            default:
+                throw new IllegalArgumentException("Invalid payment method: " + customerAnswerForPaymentMethod);
         }
 
+        PaymentMethod paymentMethod = paymentMethodFactory.createPaymentMethod(orderType);
+        orderService.orderRegister(customerName);
         //Step3 : pay price
         System.out.println("Pay Price:");
-        if(orderService instanceof OnlineOrderService){
-            orderService.onlineOrderPayment(order.getTotalPrice());
-        } else if(orderService instanceof OnSiteOrderService){
-            orderService.onSiteOrderPayment(order.getTotalPrice());
-        } else if(orderService instanceof PhoneOrderService){
-            orderService.phoneOrderPayment(order.getTotalPrice());
-        }
-
+        orderService.orderPayment(order.getTotalPrice());
         //Finally Print Bill
         System.out.println(order);
-
-
     }
-
 }
